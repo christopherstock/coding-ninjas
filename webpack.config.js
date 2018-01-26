7a618f128d0d6b2653b6a185b64844016715a5f7
Christopher Stock <email@christopherstock.de>
2018-01-26 09:18:37 +0100

    Added TS and webpack configs. Added primal yarn scripts. Added distribution index.html. Added TS entry point.

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist/js/"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [
+            ".ts",
+            ".tsx",
+            ".js",
+            ".json"
+        ]
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                loader: "source-map-loader"
+            },
+
+            // All '.css' files will be handled by the style- and css-loader
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            }
+        ]
+    }
+};
