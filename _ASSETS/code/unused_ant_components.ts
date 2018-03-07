1e9289ba67e13768193e0d0e0f8eec0a03591e59
Christopher Stock <email@christopherstock.de>
2018-03-07 16:06:36 +0100

    Primal card flip implementation.

---
+++ _ASSETS/code/unused_ant_components.ts
@@ -0,0 +1,68 @@
+
+                {
+/*
+                    ninjas.SiteContentFactory.createButton
+                    (
+                        "leftBottom",
+                        "Trigger a test alert.",
+                        "primary",
+                        "play-circle",
+                        () => { alert( "Button clicked!" ); },
+                        "Trigger a test alert"
+                    )
+
+                    ninjas.SiteContentFactory.createParagraph( "This site demonstrates the usage of the following primal packages of the NodeJS framework:" )
+
+                    ninjas.SiteContentFactory.createSpacerVertical()
+
+                    <ul className="sitePanel">
+                        <li>
+                            TypeScript<br />
+                            THE transpiler for mastering extensive JavaScript projects with plenty of own sourcecode.
+                        </li>
+                        <li>
+                            Webpack<br />
+                            THE web bundler for extensive JavaScript projects with plenty of external libraries.
+                        </li>
+                        <li>
+                            Matter.js<br />
+                            A wonderful and easy-to-use 2d physics rendering engine.
+                        </li>
+                        <li>
+                            Ant Design<br />
+                            An enterprise Framework for easy-to-use React UI-components.
+                        </li>
+                    </ul>
+
+                    <antd.Input
+                        type="text"
+                        prefix={ <antd.Icon type="user" style={ { color: 'rgba( 0, 0, 0, .25 )' } } /> }
+                        placeholder="fucker!"
+                        maxLength="37"
+                        style={ { width: "200px", } }
+                    />
+
+                    ninjas.SiteContentFactory.createParagraph( "We offer high quality enterprise and desktop application development." )
+                    ninjas.SiteContentFactory.createAvatar( "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" )
+                    ninjas.SiteContentFactory.createAvatar( "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" )
+                    ninjas.SiteContentFactory.createAvatar( "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" )
+                    ninjas.SiteContentFactory.createAvatar( "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" )
+                    ninjas.SiteContentFactory.createAvatar( "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" )
+
+                    ninjas.SiteContentFactory.createButtonGroup
+                    (
+                        "WBG",
+                        [
+                            "MUC",
+                            "BER",
+                            "WBG",
+                        ],
+                        [
+                            "Munich",
+                            "Berlin",
+                            "Würzburg",
+                        ],
+                        ( e:React.ChangeEvent<any> ) => { console.log( "Changed value to [" + e.target.value + "]" ); }
+                    )
+*/
+                }
--- src/de/mayflower/ninjas/site/component/ContentContact.tsx
+++ src/de/mayflower/ninjas/site/component/ContentContact.tsx
@@ -1,7 +1,6 @@
 
     import * as ninjas from '../../ninjas';
     import * as React  from 'react';
-    import * as antd   from 'antd';
 
     /*******************************************************************************************************************
     *   A react component with the content for the 'contact' page.
@@ -27,77 +26,42 @@
                 { ninjas.SiteContentFactory.createHeadline( "Contact Us!" ) }
                 { ninjas.SiteContentFactory.createSpacerVertical() }
 
+                <div className="card-flip-container">
+                    <div className="card-flipper">
+                        <div className="card-front">
+                            Würzburg
+                        </div>
+                        <div className="card-back">
+                            Mayflower Würzburg
+                        </div>
+                    </div>
+                </div>
+
                 { ninjas.SiteContentFactory.createSpacerVertical() }
-                { ninjas.SiteContentFactory.createParagraph( "Find out offices here.." ) }
-                { ninjas.SiteContentFactory.createDivider() }
 
-                {
-/*
-                    ninjas.SiteContentFactory.createButton
-                    (
-                        "leftBottom",
-                        "Trigger a test alert.",
-                        "primary",
-                        "play-circle",
-                        () => { alert( "Button clicked!" ); },
-                        "Trigger a test alert"
-                    )
+                <div className="card-flip-container">
+                    <div className="card-flipper">
+                        <div className="card-front">
+                            Würzburg
+                        </div>
+                        <div className="card-back">
+                            Mayflower Würzburg
+                        </div>
+                    </div>
+                </div>
 
-                    ninjas.SiteContentFactory.createParagraph( "This site demonstrates the usage of the following primal packages of the NodeJS framework:" )
+                { ninjas.SiteContentFactory.createSpacerVertical() }
 
-                    ninjas.SiteContentFactory.createSpacerVertical()
-
-                    <ul className="sitePanel">
-                        <li>
-                            TypeScript<br />
-                            THE transpiler for mastering extensive JavaScript projects with plenty of own sourcecode.
-                        </li>
-                        <li>
-                            Webpack<br />
-                            THE web bundler for extensive JavaScript projects with plenty of external libraries.
-                        </li>
-                        <li>
-                            Matter.js<br />
-                            A wonderful and easy-to-use 2d physics rendering engine.
-                        </li>
-                        <li>
-                            Ant Design<br />
-                            An enterprise Framework for easy-to-use React UI-components.
-                        </li>
-                    </ul>
-
-                    <antd.Input
-                        type="text"
-                        prefix={ <antd.Icon type="user" style={ { color: 'rgba( 0, 0, 0, .25 )' } } /> }
-                        placeholder="fucker!"
-                        maxLength="37"
-                        style={ { width: "200px", } }
-                    />
-
-                    ninjas.SiteContentFactory.createParagraph( "We offer high quality enterprise and desktop application development." )
-                    ninjas.SiteContentFactory.createAvatar( "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" )
-                    ninjas.SiteContentFactory.createAvatar( "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" )
-                    ninjas.SiteContentFactory.createAvatar( "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" )
-                    ninjas.SiteContentFactory.createAvatar( "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" )
-                    ninjas.SiteContentFactory.createAvatar( "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" )
-
-                    ninjas.SiteContentFactory.createButtonGroup
-                    (
-                        "WBG",
-                        [
-                            "MUC",
-                            "BER",
-                            "WBG",
-                        ],
-                        [
-                            "Munich",
-                            "Berlin",
-                            "Würzburg",
-                        ],
-                        ( e:React.ChangeEvent<any> ) => { console.log( "Changed value to [" + e.target.value + "]" ); }
-                    )
-*/
-                }
+                <div className="card-flip-container">
+                    <div className="card-flipper">
+                        <div className="card-front">
+                            Würzburg
+                        </div>
+                        <div className="card-back">
+                            Mayflower Würzburg
+                        </div>
+                    </div>
+                </div>
 
             </div>
         }
