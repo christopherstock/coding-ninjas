58a9766642aba7d9b0afc2e457e2c0890281f739
Christopher Stock <email@christopherstock.de>
2018-03-08 10:20:52 +0100

    Regenerated TypeDoc.

--- src/de/mayflower/ninjas/base/Main.ts
+++ src/de/mayflower/ninjas/base/Main.ts
@@ -5,21 +5,20 @@
     *   The main class contains the application's points of entry and termination.
     *
     *   TODO Light book bg and black fg.
-    *   TODO Fade book from closed to open and vice-versa (via sprite etc.)?
     *
     *   TODO Try sound error handling! (Safari etc.)
     *   TODO Test in all browsers.
-    *   TODO Regenerate TypeDoc.
     *   TODO Ditch repository!
     *   TODO Complete MVP!
     *   TODO Parallax bg elements
     *
+    *   TODO Create mobile version .. (minimum panel size and minimum canvas size 400px etc )
     *   TODO Add decoration particle effects on smashing objects / windows etc.
     *   TODO Add Camera joyride on level startup?
-    *   TODO increase player jump speed? (alter frictionAir)
+    *   TODO increase player jump speed?
     *   TODO requestAnimationFrame tryout .. matter.js - try 60 fps as a constant.
     *   TODO Sound effects for ninja girl and enemies?
-    *   TODO Ability to smash movables. (except crates etc., introduce property 'destroyable')
+    *   TODO Add destroyable movables.
     *   TODO Ask Lenz: Add file loader via npm.
     *   TODO Try 'grouce' for visualizing git project.
     *   TODO Add 'katana strike' action on ground and in air.
@@ -30,9 +29,8 @@
     *   TODO Create item pickup HUD effect!
     *   TODO Particle fx smashed crates, startup window etc.
     *   TODO Add jest tests.
-    *   TODO Add event triggers (obstacle falling down on touching trigger etc.)
+    *   TODO Add event triggers (obstacles falling down on touching trigger etc.)
     *   TODO Add cucumber tests.
-    *   TODO Create mobile version .. (minimum panel size and minimum canvas size 400px etc )
     *
     *   @author     Christopher Stock
     *   @version    1.0.0
